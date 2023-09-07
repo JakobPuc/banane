fecc78ddf5cabcf6d948a49ad949b608aa44618f
Jakob <titan.lol.com@gmail.com>
2023-09-07 21:09:02 +0200

    test

--- ESP32_tank/src/main.cpp
+++ ESP32_tank/src/main.cpp
@@ -1,102 +1,141 @@
-#include <Arduino.h>
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
 
-#include <CheapStepper.h>
+// Load Wi-Fi library
+#include <WiFi.h>
 
-// next, declare the stepper
-// and connect pins 8,9,10,11 to IN1,IN2,IN3,IN4 on ULN2003 board
+// Replace with your network credentials
+const char* ssid     = "ESP32-Access-Point";
+const char* password = "123456789";
 
-CheapStepper stepper (16,17,5,18); 
+// Set web server port number to 80
+WiFiServer server(80);
 
- // let's create a boolean variable to save the direction of our rotation
+// Variable to store the HTTP request
+String header;
 
-boolean moveClockwise = true;
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
 
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
 
 void setup() {
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
 
-  // let's set a custom speed of 20rpm (the default is ~16.25rpm)
-  
-  stepper.setRpm(5); 
-  /* Note: CheapStepper library assumes you are powering your 28BYJ-48 stepper
-   * using an external 5V power supply (>100mA) for RPM calculations
-   * -- don't try to power the stepper directly from the Arduino
-   * 
-   * accepted RPM range: 6RPM (may overheat) - 24RPM (may skip)
-   * ideal range: 10RPM (safe, high torque) - 22RPM (fast, low torque)
-   */
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
 
-  // now let's set up a serial connection and print some stepper info to the console
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
   
-  Serial.begin(9600); Serial.println();
-  Serial.print(stepper.getRpm()); // get the RPM of the stepper
-  Serial.print(" rpm = delay of ");
-  Serial.print(stepper.getDelay()); // get delay between steps for set RPM
-  Serial.print(" microseconds between steps");
-  Serial.println();
-  
-  // stepper.setTotalSteps(4076);
-  /* you can uncomment the above line if you think your motor
-   * is geared 63.68395:1 (measured) rather than 64:1 (advertised)
-   * which would make the total steps 4076 (rather than default 4096)
-   * for more info see: http://forum.arduino.cc/index.php?topic=71964.15
-   */
+  server.begin();
 }
 
-void loop() {
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-    // let's do a clockwise move first
-  
-    moveClockwise = true;
-
-    // let's move the stepper clockwise to position 2048
-    // which is 180 degrees, a half-turn (if using default of 4096 total steps)
-    
-    stepper.moveTo (moveClockwise, 2048);
-
-    // now let's print the stepper position to the console
-    
-    Serial.print("step position: ");  
-    Serial.print(stepper.getStep()); // get the current step position
-    Serial.print(" / 4096");
-    Serial.println();
-
-    // now let's wait one second
-    
-    delay(1000); // wait a sec
-
-    // and now let's move another 90 degrees (a quarter-turn) clockwise
-
-    stepper.moveDegrees (moveClockwise, 90);
-    // stepper.moveDegreesCW (90); <--- another way to do a clockwise 90 degree turn
-
-    // let's print the stepper position to the console again
-    
-    Serial.print("step position: ");
-    Serial.print(stepper.getStep());
-    Serial.print(" / 4096");
-    Serial.println();
-
-    // and wait another second
-    
-    delay(1000);
-
-    // ok, now let's reverse directions (to counter-clockwise)
-    
-    moveClockwise = false;
-
-    // and move back to the start position (0 degree)
-   
-    stepper.moveToDegree (moveClockwise, 0);
-    // moveClockwise is now false, so move counter-clockwise back to start
-
-    // let's print the position to the console once again
-    
-    Serial.print("step position: ");
-    Serial.print(stepper.getStep());
-    Serial.print(" / 4096");
-    Serial.println();
-
-    // and wait another second before starting loop() over
-    
-    delay(1000);  
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } else if (header.indexOf("GET /27/on") >= 0) {
+              Serial.println("GPIO 27 on");
+              output27State = "on";
+              digitalWrite(output27, HIGH);
+            } else if (header.indexOf("GET /27/off") >= 0) {
+              Serial.println("GPIO 27 off");
+              output27State = "off";
+              digitalWrite(output27, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
